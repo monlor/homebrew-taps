554373353a82105fb6313521650f9ce02e34aece
monlor <monlor@qq.com>
2020-01-05 23:09:16 +0800

    2020-01-05 23:09:16

--- Formula/git-push.rb
+++ Formula/git-push.rb
@@ -1,8 +1,8 @@
 class GitPush < Formula
   desc "Automatically push code to remote repository."
   homepage "https://github.com/monlor/git-push"
-  url "https://github.com/monlor/git-push/archive/v0.0.2.tar.gz"
-  sha256 "346d4fcf1783e05445d1c537701f36dd5e7b6574ab0c9e54d72ea5be72781ca0"
+  url "https://github.com/monlor/git-push/archive/v0.0.3.tar.gz"
+  sha256 "b042ec263f2ae040c89fbe5ba86b1a53d0f83381e09e6b50562f7fe2f69e73c4"
   head "https://github.com/monlor/git-push.git"
 
   def install
