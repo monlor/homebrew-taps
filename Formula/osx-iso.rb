e7e54d350153da996218042ee2a666c4d37b9519
monlor <monlor@qq.com>
2020-01-06 00:14:22 +0800

    2020-01-06 00:14:22

---
+++ Formula/osx-iso.rb
@@ -0,0 +1,15 @@
+class OsxIso < Formula
+  desc "Create a bootable ISO of OS X / macOS, from the installation app file"
+  homepage "https://github.com/busterc/osx-iso"
+  url "https://github.com/monlor/osx-iso/archive/v5.0.0.tar.gz"
+  sha256 "36435edf98e92a1c326c219263fe004e7fce121bcac24ab8e802f4dd200807cd"
+  head "https://github.com/busterc/osx-iso.git"
+
+  def install
+    bin.install "osxiso"
+  end
+
+  test do
+    File.exist?("#{bin}/osxiso")
+  end
+end
