e8b03544676f20b805b097c78cf805f939e695d7
monlor <monlor@qq.com>
2020-01-06 19:00:48 +0800

    2020-01-06 19:00:48

--- Formula/hcf-pack.rb
+++ Formula/hcf-pack.rb
@@ -1,8 +1,8 @@
 class HcfPack < Formula
   desc "Automatically publish React or Maven project to Rancher."
   homepage "https://github.com/monlor/hcf-pack"
-  url "https://github.com/monlor/hcf-pack/archive/v1.5.1.tar.gz"
-  sha256 "4d0cf86f1ac57136e000ba368fb07efcf8862897578b9744e00ef9023ab237b9"
+  url "https://github.com/monlor/hcf-pack/archive/v1.5.2.tar.gz"
+  sha256 "833f9030519ade89929e185474085840ff2d8317953057facc61cee1ce00e06b"
   head "https://github.com/monlor/hcf-pack.git"
 
   depends_on "rancher-cli" => :recommended
