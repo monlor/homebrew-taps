2a51ef9fbd3f2add2f724e572cd968c4153f0010
monlor <monlor@qq.com>
2020-01-09 10:29:51 +0800

    2020-01-09 10:29:50

--- Formula/git-merge.rb
+++ Formula/git-merge.rb
@@ -1,8 +1,8 @@
 class GitMerge < Formula
   desc "New Merge Request in GitLab."
   homepage "https://github.com/monlor/git-merge"
-  url "https://github.com/monlor/git-merge/archive/v1.1.0.tar.gz"
-  sha256 "35fcaf603ebefa8a7e5fc72410661b1f7ed0e8fb6f6a5f6cdbd3ecf98c1f6e27"
+  url "https://github.com/monlor/git-merge/archive/v1.1.1.tar.gz"
+  sha256 "2f883c6ce609d92587bd58d593fbe02cf15b2e4f8795adde416ce4721b5303b8"
   head "https://github.com/monlor/git-merge.git"
 
   def install
