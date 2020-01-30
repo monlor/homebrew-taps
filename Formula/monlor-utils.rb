605c33d22663425794f5f6d84db5dbf4f78292e6
monlor <monlor@qq.com>
2020-01-30 19:23:00 +0800

    2020-01-30 19:23:00

--- Formula/monlor-utils.rb
+++ Formula/monlor-utils.rb
@@ -1,9 +1,9 @@
 class MonlorUtils < Formula
   desc "My own utils."
-  version "v0.0.1"
+  version "v0.0.2"
   homepage "https://github.com/monlor/monlor-utils"
   url "https://github.com/monlor/monlor-utils/archive/#{version}.tar.gz"
-  sha256 "279ecb34b304172df46daecfcc98f406a54f48efc5dea138e0795b0871bbcbcf"
+  sha256 "649ac4573f821ee158bf810f067e9ce8a6fe637f7cf5a85c6c0f06f4e759e7f0"
 
   def install
     bin.install "shellchan"
