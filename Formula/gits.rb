59e3025c3182c4b10fd48e98d84699f0ee07d916
monlor <monlor@qq.com>
2020-07-21 17:54:00 +0800

    添加gits程序

---
+++ Formula/gits.rb
@@ -0,0 +1,14 @@
+class GitPush < Formula
+  desc "Enhanced git."
+  homepage "https://github.com/monlor/gits"
+  url "https://github.com/monlor/gits/archive/v0.0.2.tar.gz"
+  sha256 "5b60d73f52522b725b4c5f1a5350cc0fa91fd16c9a3168440ae6e53351202359"
+
+  def install
+    bin.install "gits"
+  end
+
+  test do
+    File.exist?("#{bin}/gits")
+  end
+end
