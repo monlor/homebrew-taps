6d23ae223bbda31c295734bbeb4f692b25a152c0
monlor <monlor@qq.com>
2020-07-22 10:13:39 +0800

    2020-07-22 10:13:39

--- Formula/gits.rb
+++ Formula/gits.rb
@@ -1,8 +1,8 @@
 class Gits < Formula
   desc "Enhanced git."
   homepage "https://github.com/monlor/gits"
-  url "https://github.com/monlor/gits/archive/v0.0.3.tar.gz"
-  sha256 "24d33b677449c0cdeb1fbe32c593e4aa77dcd75735cc696da5c999a0620e5494"
+  url "https://github.com/monlor/gits/archive/v0.0.4.tar.gz"
+  sha256 "77b6689e1e488a96d5063741e99713994e02c7b61e6aa8742bde5659e02b98e9"
 
   depends_on "github-release" => :recommended
   depends_on "helper" => :recommended
--- Formula/helper.rb
+++ Formula/helper.rb
@@ -1,8 +1,8 @@
 class Helper < Formula
   desc "Shell helper tools."
   homepage "https://github.com/monlor/shell-helper"
-  url "https://github.com/monlor/shell-helper/archive/v0.0.2.tar.gz"
-  sha256 "6a1b98fbe74ea76fe069aaf9cfa7c5dd9c723019d2e5b4b075afc4f753df065d"
+  url "https://github.com/monlor/shell-helper/archive/v0.0.3.tar.gz"
+  sha256 "828dd0d94010198bf5208e629b47bcb6d27edc6e466d732dde02cc322e0adbe7"
   head "https://github.com/monlor/shell-helper.git"
 
   def install
