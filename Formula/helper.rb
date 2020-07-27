1128c3e8f34144d2487bca9e858d4a44a9133f33
monlor <monlor@qq.com>
2020-07-27 19:46:21 +0800

    2020-07-27 19:46:21

--- Formula/helper.rb
+++ Formula/helper.rb
@@ -1,8 +1,8 @@
 class Helper < Formula
   desc "Shell helper tools."
   homepage "https://github.com/monlor/shell-helper"
-  url "https://github.com/monlor/shell-helper/archive/v0.0.6.tar.gz"
-  sha256 "704c401de78b94f7692dbb12921e14326c9acb320ea58bc078a243a7e93243f1"
+  url "https://github.com/monlor/shell-helper/archive/v0.0.7.tar.gz"
+  sha256 "0810f6c028c5f182d6b9ad3f2995624fe6c039841b8594f77a1eec2e80a79e26"
   head "https://github.com/monlor/shell-helper.git"
 
   def install
