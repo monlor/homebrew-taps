c80e7bb56bf4822c48c737a511ae188dd60b5af5
monlor <monlor@qq.com>
2022-02-08 23:25:04 +0800

    更新shell-helper

--- Formula/shell-helper.rb
+++ Formula/shell-helper.rb
@@ -1,8 +1,8 @@
 class ShellHelper < Formula
   desc "Shell helper tools."
   homepage "https://github.com/monlor/shell-helper"
-  url "https://github.com/monlor/shell-helper/archive/v0.0.10.tar.gz"
-  sha256 "6c36bc9ed8621e4fea573665576ab9122fc3685aa9ccd01fe8ce410b1f6dec66"
+  url "https://github.com/monlor/shell-helper/archive/v0.0.11.tar.gz"
+  sha256 "e2cddf9ce88cc56ce9d31eab1fac45f9beed64dae900f149e059ee484e4925b4"
   head "https://github.com/monlor/shell-helper.git"
 
   def install
