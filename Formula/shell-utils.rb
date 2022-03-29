7baf5c82ca3129dce46f6dc406e1126472ab0570
monlor <monlor@qq.com>
2022-03-29 23:04:05 +0800

    更新shellutils

--- Formula/shell-utils.rb
+++ Formula/shell-utils.rb
@@ -1,15 +1,21 @@
+$version = "v0.0.6"
+$hash = "f2bfac208300d9ec9439b95c808d788d2430112df635741f1f1fdfc60aa11177"
+
 class ShellUtils < Formula
   desc "Shell utils."
-  version "v0.0.4"
+  version $version
   homepage "https://github.com/monlor/shell-utils"
   url "https://github.com/monlor/shell-utils/archive/#{version}.tar.gz"
-  sha256 "8182a1d257be26e2faef5f43f81afb2300c28e1335e592f173b8d2bcf8ac0d5a"
+  sha256 $hash
 
-  depends_on "helper" => :recommended
+  depends_on "openconnect" => :recommended
+  depends_on "oath-toolkit" => :recommended
 
   def install
     bin.install "shellchan"
     bin.install "ghup"
+    bin.install "shellhelper"
+    bin.install "easyoc"
   end
 
 end
