8d47279776e035e958720b9f3d8c12da4d3f041f
monlor <me@monlor.com>
2022-12-13 14:32:28 +0800

    spug cli升级v0.0.4

--- Formula/spug-cli.rb
+++ Formula/spug-cli.rb
@@ -1,11 +1,10 @@
-$version = "v0.0.3"
+$version = "v0.0.4"
 
 class SpugCli < Formula
   desc "Spug command line tool."
   homepage "https://github.com/monlor/spug-cli"
   version $version
   url "https://github.com/monlor/spug-cli/archive/refs/tags/#{version}.tar.gz"
-  sha256 "0df93f5c585f2094a9d1f46521cb16158269482763dba695da98dc8b9adc8e51"
   
   depends_on "go" => :build
 
