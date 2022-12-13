4a014cd460902ce26c094788d74b0598c500386d
monlor <me@monlor.com>
2022-12-13 11:22:05 +0800

    移除sha256

--- Formula/spug-cli.rb
+++ Formula/spug-cli.rb
@@ -5,7 +5,6 @@ class SpugCli < Formula
   homepage "https://github.com/monlor/spug-cli"
   version $version
   url "https://github.com/monlor/spug-cli/archive/refs/tags/#{version}.tar.gz"
-  sha256 "13f1d74ec9d9f2c1eff28ae52536ed7b1d272253293fb661a3e78b94f736a65c"
 
   depends_on "go" => :build
 
