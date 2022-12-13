425a7317f08ef711a8d1b30c9cf48a271fe29dff
monlor <me@monlor.com>
2022-12-13 11:23:48 +0800

    添加sha256

--- Formula/spug-cli.rb
+++ Formula/spug-cli.rb
@@ -5,7 +5,8 @@ class SpugCli < Formula
   homepage "https://github.com/monlor/spug-cli"
   version $version
   url "https://github.com/monlor/spug-cli/archive/refs/tags/#{version}.tar.gz"
-
+  sha256 "0df93f5c585f2094a9d1f46521cb16158269482763dba695da98dc8b9adc8e51"
+  
   depends_on "go" => :build
 
   def install
